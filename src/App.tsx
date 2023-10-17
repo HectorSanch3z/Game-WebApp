f6b9e7ce885cf0cdd86d2bf74e81fe207b978053
Hector Sanchez <hsanquiroga16@gmail.com>
2023-10-17 12:33:28 -0600

    Props Explicados

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = ["Juarez", "Monterrey", "Mexico", "Chihuahua"];
-
+  let items = ["Juarez", "Monterrey", "Mexico", "Chihuahua"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
